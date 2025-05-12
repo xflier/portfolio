d928a6879a8d47fa41aba2a2848752d41f91955c
xflier <xflier@yahoo.com>
2025-05-12 09:46:15 -0400

    fixed a minor issue

--- user-auth-n/src/main/java/home/xflier/authn/controller/UserController.java
+++ user-auth-n/src/main/java/home/xflier/authn/controller/UserController.java
@@ -32,7 +32,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
